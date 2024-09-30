2a4ac2eb026145f077d1e370236e1613dff2db73
hitomi <chejarlabalaji2112@gmail.com>
2024-09-30 22:30:20 +0530

    added stopwatch

--- times_dates_manager.py
+++ times_dates_manager.py
@@ -1,13 +1,13 @@
 import time
 import threading
 
-class Timer():
+
+class Timer:
 	def __init__(self, duration):
 		self.duration = duration
 		self.remaining = duration
 		self.control_thread = threading.Event()
 		self.control_thread.set()
-		
 
 	def start(self):
 		threading.Thread(target = self.run).start()
@@ -18,8 +18,7 @@ class Timer():
 			time.sleep(1)
 			self.remaining -= 1
 			print("   ",self.remaining)
-		
-			
+
 	def pause(self):
 		self.control_thread.clear()
 		
@@ -32,10 +31,26 @@ class Timer():
 		threading.Thread(target = self.run).start()
 
 
- 	
+class Stopwatch:
+	def __init__(self):
+		self.marks = {}
+		self.running = False
+		self.started_at = None
+
+	def start(self):
+		self.running = True
+		self.started_at = time.perf_counter()
+
+	def mark(self, name='unknown'):
+		if self.running:
+			self.marks[name] = time.perf_counter() - self.started_at
+		else:
+			return None # handle or raise exception
+
+	def stop(self):
+		self.running = False
+		return time.perf_counter - self.started_at
 
 
-
-		
 	
 		
